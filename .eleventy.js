cb9a215b4be4926913d71a93caba76e8aa5809ed
Wilson Parson <wilson.parson@gmail.com>
2020-03-17 22:55:16 -0600

    Get started on first post

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({'_css-out': '/'});
 
+  eleventyConfig.setTemplateFormats([
+    'md',
+    'jpg'
+  ]);
+
   return {
     dir: {
       input: 'src',
       output: 'dist',
-    }
+    },
+    htmlTemplateEngine: 'njk'
   };
 }
--- src/index.md
+++ src/index.md
@@ -1,5 +1,5 @@
 ---
-layout: base
+layout: base.njk
 ---
 # wilsonparson.com
 
