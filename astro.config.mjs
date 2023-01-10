a7eed995947e70c8e59fb1d6520c343d61f4a569
Wilson Parson <wilson.parson@atomicfi.com>
2023-01-09 23:42:07 -0700

    refactor: use default layout for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import { defaultLayout } from 'astro-default-frontmatter';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [[defaultLayout, './src/layouts/MarkdownLayout.astro']],
+  },
   integrations: [
     tailwind({
       config: { path: './tailwind.config.js' },
